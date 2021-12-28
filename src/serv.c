d077f8eb697895ef80101d7712028a1678a937da
Grzegorz Płaczek <g01.placzek@gmail.com>
2021-12-28 20:38:53 +0100

    Sending and receiving channel messages

--- src/serv.c
+++ src/serv.c
@@ -22,8 +22,8 @@ int main(){
     struct query q1;
     while(1){
         msgrcv(mid, &q1, MESSAGE_SIZE, 0, 0);
-        printf("%ld\n", q1.type);
         if(q1.type == ENTER_QUERY){
+            // PODŁĄCZENIE UŻYTKOWNIKÓW DO SERWERA
             char enters = 1;
             for(int i = 0; i < nusers; i++){
                 if(!strcmp(q1.name, user_array[i].name)){ enters = 0; break; }
@@ -38,14 +38,34 @@ int main(){
                 msgsnd(mid, &q1, MESSAGE_SIZE, 0);
             }
         }else if(q1.type < 16){
+            // PRZESYŁANIE WIADOMOŚCI PRYWATNYCH
             int rec_q = msgget(user_array[q1.type - 1].pid, IPC_CREAT | 0644);
             q1.type = 2;
             msgsnd(rec_q, &q1, MESSAGE_SIZE, 0);
+        }else if(q1.type < 32){
+            // PRZESYŁANIE WIADOMOŚCI DO WSZYSTKICH UŻYTKOWNIKÓW POŁĄCZONYCH Z KANAŁEM
+            int chnl = CHANNEL_NUM(q1.type);
+            q1.type = 2; // temporary
+            printf("%d; %d\n", chnl, channel_array[chnl].n_users);
+            for(int i = 0; i < channel_array[chnl].n_users; i++){
+                struct user *temp = &(channel_array[chnl].users[i]);
+                int rec_q = msgget(temp->pid, IPC_CREAT | 0644);
+                msgsnd(rec_q, &q1, MESSAGE_SIZE, 0);
+                printf("SENT %d\n", temp->pid);
+            }
         }else if(q1.type == CHANNEL){
-            printf("%d\n", join_channel(&q1, channel_array, &nchannels));
+            // PODŁĄCZANIE UŻYTKOWNIKÓW DO KANAŁU
+            join_channel(&q1, channel_array, &nchannels);
+            for(int i = 0; i < nchannels; i++){
+                for(int j = 0; j < channel_array[i].n_users; j++){
+                    printf("%d\n", channel_array[i].users[j].pid);
+                }
+            }
         }else if(q1.type == LIST_USERS){
+            // WYSYŁANIE UŻYTKOWNIKOM LISTY UŻYTKOWNIKÓW PODŁĄCZONYCH DO SERWERA
             list_users(user_array, nusers, q1.num);
         }else if(q1.type == EXIT){
+            // ROZŁĄCZANIE UŻYTKOWNIKÓW
             int mid = msgget(q1.num, IPC_CREAT | 0644);
             q1.type = KICK;
             int to_delete;
